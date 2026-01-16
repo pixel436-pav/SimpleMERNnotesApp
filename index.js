cef5371cfe2ba74257a4fccfc61a5cd73e578d18
pavneetSingh <pavneets436@gmail.com>
2026-01-16 22:14:51 +0530

    added CORS functionality

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import noteRoutes from './routes/noteRoutes.js'
+import cors from 'cors'; // Corss origin object sharing - HTTP header based mechanism 
 
 const app = express();
 dotenv.config();
 
 //JSON parsing middleware (provided by express)
 app.use(express.json());
-const PORT = process.env.PORT || 2008
+app.use(cors());
+const PORT = process.env.PORT || 2008;
 
 mongoose.connect(process.env.MongoDbURL)
     .then(()=> console.log(`MongoDb Connected`))
